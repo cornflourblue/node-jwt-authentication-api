4cc3ad364c2c7cad3eb918403d4e86742d541a9b
jwatmore <jwatmore@ultraserve.com.au>
2020-06-08 18:39:28 +1000

    Updated to Express.js 4.17.1 and made a couple of small improvements to the user service and controller

--- users/user.service.js
+++ users/user.service.js
@@ -11,19 +11,23 @@ module.exports = {
 
 async function authenticate({ username, password }) {
     const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-        const token = jwt.sign({ sub: user.id }, config.secret);
-        const { password, ...userWithoutPassword } = user;
-        return {
-            ...userWithoutPassword,
-            token
-        };
-    }
+
+    if (!user) throw 'Username or password is incorrect';
+
+    const token = jwt.sign({ sub: user.id }, config.secret);
+    return {
+        ...omitPassword(user),
+        token
+    };
 }
 
 async function getAll() {
-    return users.map(u => {
-        const { password, ...userWithoutPassword } = u;
-        return userWithoutPassword;
-    });
+    return users.map(u => omitPassword(u));
+}
+
+// helper functions
+
+function omitPassword(user) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
 }
--- users/users.controller.js
+++ users/users.controller.js
@@ -10,12 +10,12 @@ module.exports = router;
 
 function authenticate(req, res, next) {
     userService.authenticate(req.body)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
-        .catch(err => next(err));
+        .then(user => res.json(user))
+        .catch(next);
 }
 
 function getAll(req, res, next) {
     userService.getAll()
         .then(users => res.json(users))
-        .catch(err => next(err));
+        .catch(next);
 }
